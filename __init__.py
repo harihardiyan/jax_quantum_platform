b9f79d6bba7bf04c0724543103b801e4eda8234e
harihardiyan <lorozloraz@gmail.com>
2026-01-17 01:02:21 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+JAX Quantum Platform
+A modular, research-grade quantum simulation framework.
+"""
+
+from . import core
+from . import dynamics
+from . import variational
+from . import topology
+from . import diagnostics
+from . import io
