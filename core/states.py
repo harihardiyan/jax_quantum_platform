3d564b6aea1305f152613c8679aabd2d6e109c0d
harihardiyan <lorozloraz@gmail.com>
2026-01-17 00:49:03 +0700

    Create states.py

---
+++ core/states.py
@@ -0,0 +1,19 @@
+ ---------------------------------------------------------------------------
+ModuleNotFoundError                       Traceback (most recent call last)
+/tmp/ipython-input-186704862.py in <cell line: 0>()
+     23 # ------------------------------------------------------------
+     24 from jax_quantum_platform.core.hilbert import HilbertSpace
+---> 25 from jax_quantum_platform.core.states import StateVector
+     26 from jax_quantum_platform.core.hamiltonian import StaticHamiltonian
+     27 from jax_quantum_platform.dynamics.schrodinger import time_evolve
+
+ModuleNotFoundError: No module named 'jax_quantum_platform.core.states'
+
+---------------------------------------------------------------------------
+NOTE: If your import is failing due to a missing package, you can
+manually install dependencies using either !pip or !apt.
+
+To view examples of installing some common dependencies, click the
+"Open Examples" button below.
+---------------------------------------------------------------------------
+Open Examples
